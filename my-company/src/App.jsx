06407981eaafe76dd7efe5789a8d2f4cc5e51a88
auilk <abdelghafour.lbaz01@gmail.com>
2025-02-22 17:46:18 +0100

    Finished final task

---
+++ my-company/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
