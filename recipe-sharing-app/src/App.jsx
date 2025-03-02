247c01312371e5838f79ac249283f01bc472186f
auilk <abdelghafour.lbaz01@gmail.com>
2025-03-02 05:36:25 +0000

    Finished first task

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import RecipeList from "./components/RecipeList.jsx";
+import AddRecipeForm from "./components/AddRecipeForm.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  );
+}
+
+export default App;
