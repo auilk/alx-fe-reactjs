1b9e96fd4aea74767b058bdb1fd6885246f2fd8f
auilk <abdelghafour.lbaz01@gmail.com>
2025-03-02 08:08:27 +0000

    Finished third task

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,22 +1,33 @@
 import { Link } from "react-router-dom";
 import { useRecipeStore } from "./recipeStore";
+import { useEffect } from 'react';
+import SearchBar from "./SearchBar";
 
 const RecipeList = () =>
 {
-    const { recipes } = useRecipeStore();
+    const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+    const filterRecipes = useRecipeStore(state => state.filterRecipes);
+    const searchTerm = useRecipeStore(state => state.searchTerm);
+
+    useEffect(() => { filterRecipes(); }, [searchTerm]);
 
     return (
         <div>
-        <h1>Recipe List</h1>
-        <ul>
-            {recipes.map((recipe) => (
-                <li key={recipe.id}>
-                    <Link to={`/recipes/${recipe.id}`}> {recipe.title} </Link>
-                </li>
-            ))}
-        </ul>
+            <h2>Recipes</h2>
+            <SearchBar />
+            <ul>
+                { 
+                    filteredRecipes.length > 0 ? ( filteredRecipes.map(recipe => (
+                        <li key={recipe.id}>
+                            <h3>{recipe.title}</h3>
+                            <p>{recipe.description}</p>
+                            <Link to={`/recipes/${recipe.id}`}>View Details</Link>
+                        </li>
+                    )) ) : ( <p>No recipes found.</p> )
+                }
+            </ul>
         </div>
-    );
+        );
 };
 
 export default RecipeList;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,8 +3,14 @@ import { create }  from 'zustand'
 export const useRecipeStore = create(set => (
 {
     recipes: [],
+    setRecipes: (recipes) => set({ recipes }),
     addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
     deleteRecipe: (removedRecipe) => set(state => ({ recipes: state.recipes.filter((recipe) => recipe.id !== removedRecipe) })),
     updateRecipe: (updatedRecipe) => set(state => ({ recipes: state.recipes.map((recipe) => recipe.id === updatedRecipe.id ? updatedRecipe : recipe) })),
-    setRecipes: (recipes) => set({ recipes })
+
+    searchTerm: '',
+    setSearchTerm: (term) => set({ searchTerm: term }),
+
+    filteredRecipes: [],
+    filterRecipes: () => set(state => ({ filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())) }))
 }));
