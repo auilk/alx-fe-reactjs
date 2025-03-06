dbe3d830d608f4cac955de79ff20723081963340
auilk <abdelghafour.lbaz01@gmail.com>
2025-03-06 01:42:49 +0000

    Fix

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,48 +2,61 @@ import { useState } from "react";
 
 function RegistrationForm()
 {
-    const [username, SetUsername] = useState('');
-    const [email, SetEmail] = useState('');
-    const [password, SetPassword] = useState('');
-    const [error, SetError] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({ username: false, email: false, password: false });
 
     const HandleSubmit = (event) =>
     {
         event.preventDefault();
 
-        if (!(username && email && password))
+        if (!(username))
         {
-            SetError("All field are required!");
+            setErrors((errors) => ({...errors, username: true}));
+            return;
+        }
+        if (!(email))
+        {
+            setErrors((errors) => ({...errors, email: true}));
+            return;
+        }
+        if (!(username))
+        {
+            setErrors((errors) => ({...errors, password: true}));
             return;
         }
 
         console.log("Form Submitted: ", { username, email, password });
 
-        SetUsername('');
-        SetEmail('');
-        SetPassword('');
-        SetError('');
+        setUsername('');
+        setEmail('');
+        setPassword('');
+        setErrors({ username: false, email: false, password: false });
     }
 
     return (
         <div>
             <h2>Register</h2>
             <form onSubmit={HandleSubmit}>
-                {error ? <p>{error}</p> : null}
+                {errors.username && errors.email && errors.password ? <p>All the fields are required</p> : null}
 
                 <div>
                     <label htmlFor="username">username</label>
-                    <input type="text" id="username" value={username} onChange={(event) => SetUsername(event.target.value)}/>
+                    <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)}/>
+                    {errors.username ? <p>Username is required</p> : null}
                 </div>
 
                 <div>
                     <label htmlFor="email">email</label>
-                    <input type="email" id="email" value={email} onChange={(event) => SetEmail(event.target.value)}/>
+                    <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)}/>
+                    {errors.email ? <p>Email is required</p> : null}
                 </div>
 
                 <div>
                     <label htmlFor="password">password</label>
-                    <input type="password" id="password" value={password} onChange={(event) => SetPassword(event.target.value)}/>
+                    <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+                    {errors.password ? <p>Password is required</p> : null}
                 </div>
 
                 <button type="submit">Register</button>
